46d2175fec7138568f783c4fd5a0356157b939f0
Cemb93 <karloz13m@gmail.com>
2023-01-13 23:10:50 -0500

    component presentation responsive

--- src/components/Presentation/Presentation.tsx
+++ src/components/Presentation/Presentation.tsx
@@ -8,11 +8,12 @@ export const Presentation = () => {
       <div className="presentation__description" >
         <h1>Hola! Mi nombre es <b className="presentation__name" >Carlos</b></h1>
         <h2>Soy Full Stack Developer</h2>
-        <p>Soy una persona apasionada por el mundo de la tecnología, </p>
+        <p>Soy una persona apasionada por el mundo de la tecnología, con un gran interes en seguir aprendiendo cosas nuevas, continuar mejorar mi habilidades técnicas e ir descubriendo nuevas habilidades personales y poder fortalecer las actuales.</p>
+        {/* <p>Soy una persona apasionada por el mundo de la tecnología, </p>
         <p>con un gran interes en seguir aprendiendo cosas nuevas, </p>
         <p>continuar mejorar mi habilidades técnicas </p>
         <p>e ir descubriendo nuevas habilidades personales </p>
-        <p>y poder fortalecer las actuales.</p>
+        <p>y poder fortalecer las actuales.</p> */}
       </div>
     </div>
   )
