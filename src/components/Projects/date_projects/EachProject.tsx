23fd07bd5480ed761a2b0cb6d27d5a6290733f06
Cemb93 <karloz13m@gmail.com>
2023-07-30 19:39:41 -0500

    add proyect Fitness Center Gym

--- src/components/Projects/date_projects/EachProject.tsx
+++ src/components/Projects/date_projects/EachProject.tsx
@@ -2,26 +2,26 @@
 export const projects = [
   {
     id: 1,
+    name: "Fitness Center Gym",
+    description: "Plataforma de fines administrativos donde puedes tener un control interno sobre los clientes, empleados, proveedores, elementos de gimnasia, clases disponibles y el flujo económico interno, usando tecnologías como: PostgreSQL, Node.js, TypeScript, React.js, Git, entre otras.",
+    image: './Images/Projects/Gym/Fitness Center Gym.png',
+    urlProject: "https://fit-center-gym.vercel.app/",
+    urlGit: "https://github.com/fahe8/wetravel-pf",
+  },
+  {
+    id: 2,
     name: "WeTravel",
-    description: "Este proyecto se trata de una plataforma donde podrás visualizar los alojamientos de hoteles en diferentes partes del mundo, también podrás registrar nuevos hoteles y compartir experiencias. Hecho con React, Redux, Tailwind, Express, Sequelize, PostgreSQL, entre otros.",
+    description: "Plataforma donde podrás visualizar los alojamientos de hoteles en diferentes partes del mundo,también podrás registrar nuevos hoteles y compartir experiencias, usando tecnologías como: PostgreSQL, Node.js, JavaScript, React.js, Git, entre otras tecnologías.",
     image: './Images/Projects/WeTravel/WeTravel_Landing.jpg',
     urlProject: "https://wetravel-app.vercel.app/",
     urlGit: "https://github.com/fahe8/wetravel-pf",
   },
   {
-    id: 2,
+    id: 3,
     name: "Countries",
     description: "SPA de paises en donde puedes ver información de todos los paises, puedes realizar busquedas por país, ordenar por order alfabético y según la población, filtrar por actividad y según el continente y puedes crear una actividad a los paises que consideres. Hecho en el Front-End con: HTML, CSS, TypeScript, React, Redux y desde el Back-End con: TypeScript, Nodejs, Express, Sequelize y PostgreSQL.",
     image: './Images/Projects/Countries/Countries.jpg',
     urlProject: "https://countries-ts-cemb.netlify.app/",
     urlGit: "https://github.com/Cemb93/Countries",
   },
-  // {
-  //   id: 3,
-  //   name: "VideoGames",
-  //   description: "SPA de video juegos en donde puedes ver información de 100 video juegos, puedes realizar busquedas por juego, ordenar por order alfabético, filtrar por genero y puedes crear tu propio video juego. Hecho con HTML, CSS, JavaScript, React, Redux, Nodejs, Express, Sequelize y PostgreSQL.",
-  //   image: './Images/Projects/WeTravel/WeTravel_Landing.jpg',
-  //   urlProject: "",
-  //   urlGit: "",
-  // },
 ];
